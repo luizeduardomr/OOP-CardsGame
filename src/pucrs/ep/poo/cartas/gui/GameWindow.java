a6b3edf4a5f05c409e02b2cb19dcaeec510c5d74
Roberto <robertodermato@gmail.com>
2019-11-02 10:33:29 -0300

    criada tabpane com tab1 contendo o jogo

--- src/pucrs/ep/poo/cartas/gui/GameWindow.java
+++ src/pucrs/ep/poo/cartas/gui/GameWindow.java
@@ -7,11 +7,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Button;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.event.*;
 import pucrs.ep.poo.cartas.modelo.Game;
@@ -31,21 +31,35 @@ public class GameWindow extends Application implements Observer{
 
         Group root = new Group();
 
+        TabPane tabPane = new TabPane();
+
+        Tab tab1 = new Tab("Planes", new Label("Show all planes available"));
+        Tab tab2 = new Tab("Cars"  , new Label("Show all cars available"));
+        Tab tab3 = new Tab("Boats" , new Label("Show all boats available"));
+
+        tabPane.getTabs().add(tab1);
+        tabPane.getTabs().add(tab2);
+        tabPane.getTabs().add(tab3);
+
+
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-       
+
+
         DeckView deckJ1 = new DeckView(1);
         ScrollPane sd1 = new ScrollPane();
-        sd1.setPrefSize(950, 320);
+        sd1.setPrefSize(1200, 400);
         sd1.setContent(deckJ1);
         grid.add(sd1,0,0);
-        
+
+
         PlacarView placar = new PlacarView();
         grid.add(placar,0,1);
-        
+
+
         Button butClean = new Button("Clean");
         grid.add(butClean,1,1);
         butClean.setOnAction(new EventHandler<ActionEvent>() {
@@ -57,15 +71,19 @@ public class GameWindow extends Application implements Observer{
 
         DeckView deckJ2 = new DeckView(2);
         ScrollPane sd2 = new ScrollPane();
-        sd2.setPrefSize(950, 320);
+        sd2.setPrefSize(1200, 400);
         sd2.setContent(deckJ2);
         grid.add(sd2,0,2);
 
+        tab1.setContent(grid);
+
         ObservableList list = root.getChildren();
-        list.add(grid);
+        //list.add(grid);
+        list.add(tabPane);
 
         Scene scene = new Scene(root);
-        primaryStage.setScene(scene);        
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
