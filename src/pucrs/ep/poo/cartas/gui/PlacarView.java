74d4b89cc1f0adf31c1f3aaf6772086c0b91944a
19180346 <mariane1>
2019-11-13 19:47:56 -0200

    pontos e reserva de mana

--- src/pucrs/ep/poo/cartas/gui/PlacarView.java
+++ src/pucrs/ep/poo/cartas/gui/PlacarView.java
@@ -10,6 +10,7 @@ import pucrs.ep.poo.cartas.modelo.Game;
 
 public class PlacarView extends GridPane implements Observer{
     private TextField ptsJ1,ptsJ2;
+    private TextField manaJ1, manaJ2;
     
     public PlacarView(){
         this.setAlignment(Pos.CENTER);
@@ -21,20 +22,31 @@ public class PlacarView extends GridPane implements Observer{
         
         ptsJ1 = new TextField();
         ptsJ2 = new TextField();
+        manaJ1 = new TextField();
+        manaJ2 = new TextField();
       
         ptsJ1.setText(""+Game.getInstance().getLifeJ1());
         ptsJ2.setText(""+Game.getInstance().getLifeJ2());
+        manaJ1.setText(""+Game.getInstance().getManaReserveJ1());
+        manaJ2.setText(""+Game.getInstance().getManaReserveJ2());
 
-        this.add(new Label("Jogador 1:"),0,0);
+        this.add(new Label("Jogador 1 - Vida:"),0,0);
         this.add(ptsJ1,1,0);
-        this.add(new Label("Jogador 2:"),0,1);
+        this.add(new Label("Mana:"),2,0);
+        this.add(manaJ1,3,0);
+
+        this.add(new Label("Jogador 2 - Vida:"),0,1);
         this.add(ptsJ2,1,1);
+        this.add(new Label("Mana:"),2,1);
+        this.add(manaJ2,3,1);
     }
     
     @Override
     public void update(Observable o,Object arg){
         ptsJ1.setText(""+Game.getInstance().getLifeJ1());
         ptsJ2.setText(""+Game.getInstance().getLifeJ2());
+        manaJ1.setText(""+Game.getInstance().getManaReserveJ1());
+        manaJ2.setText(""+Game.getInstance().getManaReserveJ2());
     }    
 }
 
