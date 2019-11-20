70c3eb15ac189c3491c87d78e6cdb84a91aaead5
Luiz Eduardo <luizdudumr@gmail.com>
2019-11-20 11:57:58 -0300

    Adição de criaturas no deck vermelho

--- src/pucrs/ep/poo/cartas/modelo/RedCreatures.java
+++ src/pucrs/ep/poo/cartas/modelo/RedCreatures.java
@@ -30,6 +30,27 @@ public class RedCreatures {
         CreatureCard mek = new CreatureCard("mek", "mekimg", 1,1,1, CreatureCard.Colour.RED);
         creatures.add(mek);
 
+        CreatureCard amor = new CreatureCard("amor", "amorimg", 3, 3, 2, CreatureCard.Colour.RED);
+        creatures.add(amor);
+
+        CreatureCard Magnata = new CreatureCard("magnata", "magnataimg", 5,6,4, CreatureCard.Colour.RED);
+        creatures.add(Magnata);
+
+        CreatureCard forca = new CreatureCard("forca", "forcaimg", 2,2,2, CreatureCard.Colour.RED);
+        creatures.add(forca);
+
+        CreatureCard IntelliJ = new CreatureCard("IntelliJ", "intellijimg", 5,5,4, CreatureCard.Colour.RED);
+        creatures.add(IntelliJ);
+
+        CreatureCard mago = new CreatureCard("mago", "magoimg", 3,3,3, CreatureCard.Colour.RED);
+        creatures.add(mago);
+
+        CreatureCard mexicans = new CreatureCard("mexicans", "meximg", 4,4,3, CreatureCard.Colour.RED);
+        creatures.add(mexicans);
+
+        CreatureCard ra = new CreatureCard("ra", "raimg", 1,2,1, CreatureCard.Colour.RED);
+        creatures.add(ra);
+
 
 
     }
